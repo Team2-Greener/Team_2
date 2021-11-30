dd8231c6f7366dcb12f947d12151aa0ae51aa570
Hyein Seo <appleshi@naver.com>
2021-12-01 02:33:40 +0900

    shop review 완료.

--- app/src/main/java/com/greener/StoreDetailView.java
+++ app/src/main/java/com/greener/StoreDetailView.java
@@ -1,29 +1,30 @@
 package com.greener;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,11 +35,11 @@ import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Marker;
-import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.overlay.OverlayImage;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class StoreDetailView extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
@@ -47,13 +48,17 @@ public class StoreDetailView extends AppCompatActivity implements View.OnClickLi
     private OverlayImage image = OverlayImage.fromResource(R.drawable.ic_place_marker);
 
     private ArrayList<String> arrayList;
+    private ArrayList<ReviewList> arrayList_show_review;
     private FirebaseDatabase database, likedDatabase, saveDatabase;
-    private DatabaseReference databaseReference, likedReference, saveReference;
-    private RecyclerView.Adapter adapter;
+    private DatabaseReference databaseReference, likedReference, databaseReference_show_review, saveReference;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();;
+    private RecyclerView.Adapter adapter, adapter_show_review;
     private ViewPager2 sliderViewPager;
     private LinearLayout layoutIndicator;
+    private RecyclerView recyclerView_show_review;
+    private RecyclerView.LayoutManager layoutManager, layoutManager_show_review;
 
-    private Button Back, Review, Map;
+    private Button Back, Review, Map, write_reivew;
     private FrameLayout mapFrame,reviewFrame;
     private static Button Save;
 
@@ -61,6 +66,8 @@ public class StoreDetailView extends AppCompatActivity implements View.OnClickLi
     private String Image, path;
     private Double x, y;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,6 +77,7 @@ public class StoreDetailView extends AppCompatActivity implements View.OnClickLi
         Map = findViewById(R.id.store_detail_map);
         Review = findViewById(R.id.store_detail_review);
         Save = findViewById(R.id.btn_to_save);
+        write_reivew = findViewById(R.id.btn_write_review);
 
         mapFrame = findViewById(R.id.map_frame);
         reviewFrame = findViewById(R.id.review_frame);
@@ -193,6 +201,95 @@ public class StoreDetailView extends AppCompatActivity implements View.OnClickLi
         });
 
         setupIndicators(arrayList.size());
+
+        recyclerView_show_review = findViewById(R.id.review_recyclerview);
+        recyclerView_show_review.setHasFixedSize(true);     //리사이클러뷰 성능 강화
+        layoutManager_show_review = new LinearLayoutManager(this);
+        recyclerView_show_review.setLayoutManager(layoutManager_show_review);
+
+        arrayList_show_review = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        databaseReference_show_review = database.getReference("reviews"); // DB 테이블 연결
+        databaseReference_show_review.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList_show_review.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    ReviewList review = snapshot.getValue(ReviewList.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList_show_review.add(review); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter_show_review.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+                Log.d("SIZE : ",String.valueOf(adapter_show_review.getItemCount()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("StoreDetailView_review", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+        //Toast.makeText(this,arrayList_show_review.size(),Toast.LENGTH_SHORT).show();
+        adapter_show_review = new ReviewViewAdapter(arrayList_show_review, this);
+        recyclerView_show_review.setAdapter(adapter_show_review); // 리사이클러뷰에 어댑터 연결
+
+        LocalDateTime now = LocalDateTime.now();
+        String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+        write_reivew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final LinearLayout linear = (LinearLayout) View.inflate(StoreDetailView.this, R.layout.dialog_review, null);
+                new AlertDialog.Builder(StoreDetailView.this) .setView(linear)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText url = (EditText) linear.findViewById(R.id.edit_review);
+                                String value = url.getText().toString();
+                                ReviewList review = new ReviewList();
+                                //review uid 저장
+                                review.setUid(firebaseAuth.getUid());
+                                //review name 저장
+                                databaseReference = database.getReference("users"); // DB 테이블 연결
+                                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                                            UsersList u = snapshot.getValue(UsersList.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                                            if(u.getUid().equals(firebaseAuth.getUid())) {
+                                                int size = u.getName().length();
+                                                String username = u.getName().substring(0,3);
+                                                String result=username.concat("****");
+                                                review.setUsername(result);
+                                                review.setText(value);
+                                                dialog.dismiss();
+                                                // database에 저장
+                                                String path = "reviews/"+now.format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH:mm:ss"));;
+                                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(path); // DB 테이블 연결
+                                                databaseReference.setValue(review);
+                                            }
+                                        }
+                                    }
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        // 디비를 가져오던중 에러 발생 시
+                                        Log.e("StoreDetailView_edit", String.valueOf(databaseError.toException())); // 에러문 출력
+                                    }
+                                });
+
+
+
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
     }
 
     private void setupIndicators(int count) {
